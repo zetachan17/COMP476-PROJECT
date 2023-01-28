1f73b942f0cd6fabb61973469a89e4de7d1f43c9
Shadi <51339220+shadijiha@users.noreply.github.com>
2023-01-27 19:56:46 -0500

    Update PowerUp.cs

--- Assets/Stat system/PowerUp.cs
+++ Assets/Stat system/PowerUp.cs
@@ -1,5 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.Events;
 using UnityEngine;
 
 public class PowerUp : MonoBehaviour
@@ -8,11 +8,24 @@ public class PowerUp : MonoBehaviour
     protected StatTracked.Stat modifies;
 
     [SerializeField]
-    protected float newValue;   // This can also be a function if we want the new value to depend on the old value
+    protected float newValue;   // We make this a function if we want the new value to depend on the old value
 
+    [SerializeField]
+    protected float duration;   // Duration of the powerup in seconds.\
+
+    [SerializeField]
+    protected UnityEvent<StatTracked> OnPowerUpApplied;
+
+    [SerializeField]
+    protected UnityEvent<StatTracked> OnPowerUpExpired;
 
     public void Apply(StatTracked obj) {
+        var oldStat = obj.GetStat(modifies);
         obj.SetStat(modifies, newValue);
+        OnPowerUpApplied?.Invoke(obj);
+        StartCoroutine(Wait());
+        obj.SetStat(modifies, oldStat);
+        OnPowerUpExpired?.Invoke(obj);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -20,5 +33,9 @@ public class PowerUp : MonoBehaviour
         StatTracked tracker = collision.gameObject.GetComponent<StatTracked>();
         if (tracker != null)
             Apply(tracker);
+    }
+
+    private IEnumerator Wait() { 
+        yield return new WaitForSeconds(duration);
     }
 }
