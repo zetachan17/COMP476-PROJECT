c2c7bc7e5168c37a65b1c18a7bdfbfae3e406d09
Runze Zhu <z_runze@live.concordia.ca>
2023-03-15 23:23:44 -0400

    basic player control

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,14 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // private PlayerInput _playerInput;
-    // private InputAction _shootAction;
-    //
-    //
-    // private void Awake()
-    // {
-    //     _playerInput = GetComponent<PlayerInput>();
-    //     _shootAction = _playerInput.actions["Shoot"];
-    //     _shootAction.ReadValue<float>();
-    // }
-
     private PlayerControls _playerControls;
+    
+    [SerializeField] private GameObject target;
+    [SerializeField] private float maxVelocity = 3f;
+    [SerializeField] private float maxForce = 15f;
+    
+    private Vector3 velocity;
+    private float decreaseSpeed = 0.1f;
     
     private void Awake()
     {
@@ -36,10 +32,40 @@ public class PlayerController : MonoBehaviour
     
     void Start()
     {
+        velocity = Vector3.zero;
+        
         _playerControls.Newactionmap.Move.performed += Move;
         _playerControls.Newactionmap.Shoot.performed += Shoot;
     }
+    
+    void Update()
+    {
+    //     //if (_target != null)
+    //     //{
+    //         var desiredVelocity = (_target.transform.position - transform.position).normalized * _maxVelocity;
+    //         
+    //         var steering = desiredVelocity - velocity;
+    //         steering = Vector3.ClampMagnitude(steering, _maxForce);
+    //         
+    //         velocity = Vector3.ClampMagnitude(velocity + steering, _maxVelocity);
+    //         transform.position += velocity * Time.deltaTime;
+    //         transform.forward = velocity.normalized;
+    //         
+    //         Debug.DrawRay(transform.position, velocity.normalized * 2, Color.green);
+    //         Debug.DrawRay(transform.position, steering.normalized * 2, Color.red);
+    //     //}
+        Vector3 move = _playerControls.Newactionmap.Move.ReadValue<Vector3>();
+        velocity = move * maxVelocity;
+        
+        //SlowDown(move);
 
+        transform.position += velocity * Time.deltaTime;
+        
+        if (velocity != Vector3.zero)
+            transform.forward = velocity.normalized;
+
+    }
+    
     private void Move(InputAction.CallbackContext context)
     {
         Vector3 move = _playerControls.Newactionmap.Move.ReadValue<Vector3>();
@@ -49,5 +75,20 @@ public class PlayerController : MonoBehaviour
     public void Shoot(InputAction.CallbackContext context)
     {
         Debug.Log("Shoot");
+    }
+    
+    private void SlowDown(Vector3 move)
+    {
+        if (move == Vector3.zero)
+        {
+            float x = Mathf.Lerp(velocity.x, 0.0f, Time.deltaTime * decreaseSpeed);
+            float y = Mathf.Lerp(velocity.y, 0.0f, Time.deltaTime * decreaseSpeed);
+            float z = Mathf.Lerp(velocity.z, 0.0f, Time.deltaTime * decreaseSpeed);
+            velocity = new Vector3(x, y, z);
+            
+            if (velocity.magnitude < 0.01f) {
+                velocity = Vector3.zero;
+            }
+        }
     }
 }
