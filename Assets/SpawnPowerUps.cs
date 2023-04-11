a1cf4c12f6e7ef43aa1b6f3addca06813ec676a5
SamuelDesktop <s_lambert-senecal@hotmail.fr>
2023-04-10 21:05:17 -0400

    fix a small problem in power up

--- Assets/SpawnPowerUps.cs
+++ Assets/SpawnPowerUps.cs
@@ -31,7 +31,7 @@ public class SpawnPowerUps : MonoBehaviour
             Vector3 spawnPosition = new Vector3(x + nodePosition.x, y + nodePosition.y, z + nodePosition.z);
             int numOfPowerUps = powerUpList.Count;
 
-           GameObject newPowerUp = Instantiate(powerUpList[Random.Range(0, numOfPowerUps-1)], spawnPosition, Quaternion.identity);
+           GameObject newPowerUp = Instantiate(powerUpList[Random.Range(0, numOfPowerUps)], spawnPosition, Quaternion.identity);
             newPowerUp.GetComponent<PowerUp>().SelfDestroy();
         }
     }
